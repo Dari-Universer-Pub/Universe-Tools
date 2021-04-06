5b65b39c8859022689991b2e3a9c8e68b195346d
Dari-Universer-Pub <80520715+Dari-Universer-Pub@users.noreply.github.com>
2021-04-06 08:25:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,29 +10,16 @@ Client.on("ready", () => {
 
 Client.on("guildMemberAdd", member => {
     console.log("Un nouveau membre est arrivé");
-    member.guild.channels.cache.find(channel => channel.id === "804006373783502949").send(member.displayName + "est arrivé !");
 });
 
 Client.on("guildMemberRemove", member => {
     console.log("Un nouveau membre nous a quitté");
-    member.guild.channels.cache.find(channel => channel.id === "815600325024284672").send(member.displayName + "nous a quitté !");
 });
 
-Client.on("messageReactionAdd", (reaction, user) => {
-    console.log("reaction ajouté");
-});
-
-Client.on("messageReactionRemove", (reaction, user) => {
-    console.log("reaction retiré");
-});  
-    
 Client.on("message", message => {
     if(message.author.bot) return; 
-    if(message.channel.type == "dm") return;
-    
-    message.react("🙌");
-    message.react("820991309753876511")
-    
+
+
     if(message.content == prefix + "ping"){
         message.channel.send("pong");
     }
