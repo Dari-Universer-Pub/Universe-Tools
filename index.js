a9a60d272a0118869e0a6bd2e6b9f5de28605600
Dari-Universer-Pub <80520715+Dari-Universer-Pub@users.noreply.github.com>
2021-04-06 13:58:04 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js')
+const bot = new Discord.Client()
 
-const Client = new Discord.Client;
+bot.on('ready', function () {
+    console.log("Je suis connecté !")
+    bot.user.setActivity('Undertale', { type: "PLAYING" });
+}) 
 
-const prefix = "U!";
+bot.on('message', message => {
+  if (message.content === 'ping') {
+    message.reply('pong !')
+  }
+})
 
-Client.on("ready", () => {
-    console.log("bot opérationnel");
-});
-
-Client.on("message", message => {
-    if(message.author.bot) return; 
-    if(message.channel.type == "dm") return;
-    
-    if(message.member.hasPermission("ADMINISTRATOR")){
-        if(message.content.startsWith(prefix + "ban")){
-            let mention = message.mentions.members.first();
-        
-             if(mention == undefined){
-                 message.reply("Membre non ou mal mentionné.");
-             }
-             else {
-                 if(mention.bannable){
-                     mention.ban();
-                     message.channel.send(mention.displayName + " a été banni avec succès");
-                 }
-                 else {
-                     message.reply("Impossible de bannir ce membre.");
-                 }
-             }
-         }
-         else if(message.content.startsWith(prefix + "kick")){
-             let mention = message.mentions.members.first();
-
-             if(mention = undefined){
-                 message.reply("Membre non ou mal mentionné.");
-             } 
-             else {
-                 if(mention.kickable){
-                     mention.kick();
-                     message.channel.send(mention.displayName + " a été kick avec succès.");
-                 }
-                 else {
-                     message.reply("Impossible de kick ce membre.");
-                 } 
-             } 
-         }
-     }
-});
-
-
-Client.login(process.env.BOT_TOKEN)
+bot.login(process.env.BOT_TOKEN)
